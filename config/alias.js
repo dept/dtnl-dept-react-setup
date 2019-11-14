9723090e120c65c1eabbda717cbd791e73c1f9b4
Tim de Wolf <tpdewolf@gmail.com>
2019-11-14 13:46:56 +0100

    Get aliases from tsconfig

--- config/alias.js
+++ config/alias.js
@@ -1,9 +1,23 @@
+const tsconfig = require('../tsconfig.json')
 const path = require('path')
 
+// get aliases from tsconfig
+const aliases = tsconfig.compilerOptions.paths
+const aliasKeys = Object.keys(aliases)
+
+const stripSlashStar = string => string.replace('/*', '')
+
 function setAliasConfig(config) {
-  config.resolve.alias['@'] = path.join(process.cwd(), 'src')
-  config.resolve.alias['@static'] = path.join(process.cwd(), 'public/static')
-  config.resolve.alias['@server'] = path.join(process.cwd(), 'server')
+  const webpackAliases = aliasKeys.reduce((obj, key) => {
+    const aliasPath = stripSlashStar(aliases[key][0]).replace('./', '')
+    obj[stripSlashStar(key)] = path.join(process.cwd(), aliasPath)
+    return obj
+  }, {})
+
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    ...webpackAliases,
+  }
 }
 
 module.exports = {
