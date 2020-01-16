92481918824b96849941f06db738b9906784956c
Tim de Wolf <tpdewolf@gmail.com>
2020-01-16 14:50:28 +0100

    update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,12 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/indent': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
     '@typescript-eslint/no-object-literal-type-assertion': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/camelcase': 'off',
