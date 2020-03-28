d3dac73ef7bd58457760dc45394e4384e2e9db6e
Tim de Wolf <tpdewolf@gmail.com>
2020-03-28 17:47:41 +0100

    add test folder and alias

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,17 @@
 module.exports = {
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-    '!**/.storybook/**',
-    '!**/storybook-static/**',
-  ],
-  setupFilesAfterEnv: ['<rootDir>/config/jest/setupEnv.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/storybook/', '/_templates/'],
+  collectCoverageFrom: ['(src|server)/**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+  setupFilesAfterEnv: ['<rootDir>/test/config/setupEnv.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  roots: ['<rootDir>/src/', '<rootDir>/server/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^.+\\.css$': '<rootDir>/test/config/cssTransform.js',
   },
   transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
     '@static/(.*)$': '<rootDir>/public/static/$1',
+    '@test/(.*)$': '<rootDir>/test/$1',
     '@server/(.*)$': '<rootDir>/server/$1',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
