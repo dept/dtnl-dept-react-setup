c3f95ae10d93fa180a123f6ff664cdf20491fa4b
Tim de Wolf <tpdewolf@gmail.com>
2020-04-19 21:30:28 +0200

    update eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
+/**
+ * The lint rules are extended from `dept-react-scripts`.
+ * You can add or overwrite rules if you want to.
+ *
+ * If you wish to eject from `dept-react-script` run `npx dept-react eslint-eject`
+ */
+
 module.exports = {
   extends: ['./node_modules/dept-react-scripts/config/eslint'],
 }
