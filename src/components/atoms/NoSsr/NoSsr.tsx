c143b481f72408a2ff5e4117e8b7d7a4aa4b29cd
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2020-04-30 22:05:34 +0200

    adjust

--- src/components/atoms/NoSsr/NoSsr.tsx
+++ src/components/atoms/NoSsr/NoSsr.tsx
@@ -3,7 +3,11 @@ import { FC } from 'react'
 export interface Props {}
 
 export const NoSsr: FC<Props> = ({ children }) => {
-  if (!process.browser) return null
+  /**
+   * to validate whenever your code is running either client side or in node, use typeof window
+   * https://github.com/zeit/next.js/pull/7651
+   */
+  if (typeof window === 'undefined') return null
 
   return <>{children}</>
 }
