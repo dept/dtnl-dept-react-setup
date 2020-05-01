4c2690824628199edc22cee9d90c09e6d1171c2f
Tim de Wolf <tpdewolf@gmail.com>
2020-05-01 17:53:35 +0200

    feat(box): add pseudobox from Chakra UI
    Add pseudobox that allows for pseudo statements like hover or focus

--- src/components/atoms/Grid/Box.tsx
+++ src/components/atoms/Grid/Box.tsx
@@ -1,80 +1,226 @@
-import { HTMLAttributes } from 'react'
-import styled from 'styled-components'
+import styled, { DefaultTheme } from 'styled-components'
 import {
   background,
+  BackgroundImageProps,
+  BackgroundPositionProps,
   BackgroundProps,
+  BackgroundSizeProps,
   border,
-  BorderProps,
-  ButtonStyleProps,
+  BorderColorProps,
+  BorderRadiusProps,
+  BordersProps,
+  BoxShadowProps,
   color,
   ColorProps,
-  colorStyle,
-  ColorStyleProps,
   compose,
+  DisplayProps,
   flexbox,
   FlexboxProps,
+  FlexDirectionProps,
+  FontSizeProps,
+  FontWeightProps,
+  grid,
+  GridProps,
+  HeightProps,
   layout,
   LayoutProps,
-  opacity,
+  LetterSpacingProps,
+  LineHeightProps,
+  MaxHeightProps,
+  MaxWidthProps,
+  MinHeightProps,
+  MinWidthProps,
   OpacityProps,
+  OverflowProps,
   position,
   PositionProps,
+  ResponsiveValue,
   shadow,
   ShadowProps,
   space,
   SpaceProps,
-  style,
-  textStyle,
-  TextStyleProps,
   typography,
-  TypographyProps,
+  TypographyProps as SSTypographyProps,
+  WidthProps,
 } from 'styled-system'
 
-type TextDecorationOption = 'overline' | 'line-through' | 'underline'
-type TextTransformOption = 'uppercase' | 'lowercase' | 'capitalize'
+import { boxConfig } from './boxConfig'
 
-export type BoxProps = BackgroundProps &
-  ButtonStyleProps &
+type CSS = React.CSSProperties
+
+type borderRadius = BorderRadiusProps['borderRadius']
+type borderColor = BorderColorProps['borderColor']
+type FontSizes = keyof DefaultTheme['fontSizes']
+type FontWeights = keyof DefaultTheme['fontWeights']
+type LineHeights = keyof DefaultTheme['lineHeights']
+type LetterSpacings = keyof DefaultTheme['letterSpacings']
+
+export interface CustomConfig {
+  // Custom borderRadius alias
+  rounded?: borderRadius
+  roundedTop?: borderRadius
+  roundedBottom?: borderRadius
+  roundedLeft?: borderRadius
+  roundedRight?: borderRadius
+  roundedTopRight?: borderRadius
+  roundedTopLeft?: borderRadius
+  roundedBottomRight?: borderRadius
+  roundedBottomLeft?: borderRadius
+
+  // Custom borderColor alias
+  borderBottomColor?: borderColor
+  borderTopColor?: borderColor
+  borderRightColor?: borderColor
+  borderLeftColor?: borderColor
+
+  // Custom width alias
+  w?: WidthProps['width']
+  minW?: MinWidthProps['minWidth']
+  maxW?: MaxWidthProps['maxWidth']
+
+  // Custom height alias
+  h?: HeightProps['height']
+  minH?: MinHeightProps['minHeight']
+  maxH?: MaxHeightProps['maxHeight']
+
+  // Custom display alias
+  d?: DisplayProps['display']
+
+  // Custom background alias
+  backgroundAttachment?: ResponsiveValue<CSS['backgroundAttachment']>
+  bgImg?: BackgroundImageProps['backgroundImage']
+  bgImage?: BackgroundImageProps['backgroundImage']
+  bgSize?: BackgroundSizeProps['backgroundSize']
+  bgPos?: BackgroundPositionProps['backgroundPosition']
+  pos?: PositionProps['position']
+  flexDir?: FlexDirectionProps['flexDirection']
+
+  // CSS properties
+  textDecoration?: ResponsiveValue<CSS['textDecoration']>
+  textDecor?: ResponsiveValue<CSS['textDecoration']>
+  textTransform?: ResponsiveValue<CSS['textTransform']>
+  overflowX?: OverflowProps['overflow']
+  overflowY?: OverflowProps['overflow']
+  appearance?: ResponsiveValue<CSS['appearance']>
+  transform?: ResponsiveValue<CSS['transform']>
+  transformOrigin?: ResponsiveValue<CSS['transformOrigin']>
+  animation?: ResponsiveValue<CSS['animation']>
+  userSelect?: ResponsiveValue<CSS['userSelect']>
+  pointerEvents?: ResponsiveValue<CSS['pointerEvents']>
+  boxSizing?: ResponsiveValue<CSS['boxSizing']>
+  cursor?: ResponsiveValue<CSS['cursor']>
+  resize?: ResponsiveValue<CSS['resize']>
+  transition?: ResponsiveValue<CSS['transition']>
+  objectFit?: ResponsiveValue<CSS['objectFit']>
+  objectPosition?: ResponsiveValue<CSS['objectPosition']>
+  visibility?: ResponsiveValue<CSS['visibility']>
+
+  // Ellipsis alias
+  wordBreak?: ResponsiveValue<CSS['wordBreak']>
+  overflowWrap?: ResponsiveValue<CSS['overflowWrap']>
+  whiteSpace?: ResponsiveValue<CSS['whiteSpace']>
+
+  // SVG color properties
+  fill?: ColorProps['color']
+  stroke?: ColorProps['color']
+
+  bgAttachment?: ResponsiveValue<CSS['backgroundAttachment']>
+  shadow?: BoxShadowProps['boxShadow']
+
+  // List properties
+  listStyleType?: ResponsiveValue<CSS['listStyleType']>
+  listStylePosition?: ResponsiveValue<CSS['listStylePosition']>
+  listStyleImage?: ResponsiveValue<CSS['listStyleImage']>
+  listStyleImg?: ResponsiveValue<CSS['listStyleImage']>
+  listStylePos?: ResponsiveValue<CSS['listStylePosition']>
+
+  // Outline prop
+  outline?: ResponsiveValue<CSS['outline']>
+  float?: ResponsiveValue<CSS['float']>
+  willChange?: ResponsiveValue<CSS['willChange']>
+
+  // Border Width props
+  borderTopWidth?: ResponsiveValue<CSS['borderTopWidth']>
+  borderBottomWidth?: ResponsiveValue<CSS['borderBottomWidth']>
+  borderLeftWidth?: ResponsiveValue<CSS['borderLeftWidth']>
+  borderRightWidth?: ResponsiveValue<CSS['borderRightWidth']>
+}
+
+export interface FontSize {
+  fontSize?: ResponsiveValue<FontSizes> | FontSizeProps['fontSize']
+}
+
+export interface FontWeight {
+  fontWeight?: ResponsiveValue<FontWeights> | FontWeightProps['fontWeight']
+}
+
+export interface LineHeight {
+  lineHeight?: ResponsiveValue<LineHeights> | LineHeightProps['lineHeight']
+}
+
+export interface LetterSpacing {
+  letterSpacing?: ResponsiveValue<LetterSpacings> | LetterSpacingProps['letterSpacing']
+}
+
+type TypographyProps = Omit<
+  SSTypographyProps,
+  'fontWeight' | 'lineHeight' | 'fontSize' | 'letterSpacing'
+>
+
+export interface Truncated {
+  /**
+   * If `true`, the text will be truncated
+   */
+  isTruncated?: boolean
+}
+
+type StyledSystemProps = LayoutProps &
   ColorProps &
-  BorderProps &
-  ColorStyleProps &
-  FlexboxProps &
-  LayoutProps &
-  OpacityProps &
-  PositionProps &
-  ShadowProps &
   SpaceProps &
-  TextStyleProps &
-  TypographyProps & {
-    textDecoration?: TextDecorationOption | (TextDecorationOption | null | string)[]
-    textTransform?: TextTransformOption | (TextTransformOption | null | string)[]
-  } & HTMLAttributes<any>
+  BordersProps &
+  BackgroundProps &
+  PositionProps &
+  FlexboxProps &
+  ShadowProps &
+  GridProps &
+  OpacityProps &
+  OverflowProps
 
-const textDecoration = style({
-  prop: 'textDecoration',
-  cssProperty: 'textDecoration',
-})
+type ModifiedStyledSystemProps = TypographyProps &
+  FontSize &
+  LetterSpacing &
+  FontWeight &
+  LineHeight &
+  CustomConfig
 
-const textTransform = style({
-  prop: 'textTransform',
-  cssProperty: 'textTransform',
-})
+type BoxHTMLProps = React.RefAttributes<any> & React.HTMLAttributes<any>
 
-const boxStyles = compose(
+export type BoxProps = BoxHTMLProps & StyledSystemProps & ModifiedStyledSystemProps & Truncated
+
+export const truncate = (props: BoxProps) => {
+  if (props.isTruncated) {
+    return {
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      whiteSpace: 'nowrap',
+    }
+  }
+
+  return null
+}
+
+export const boxStyles = compose(
+  layout,
+  color,
+  space,
   background,
   border,
-  color,
-  colorStyle,
-  flexbox,
-  layout,
-  opacity,
+  grid,
   position,
   shadow,
-  space,
-  textDecoration,
-  textStyle,
-  textTransform,
   typography,
+  flexbox,
+  boxConfig,
 )
 
 export const Box = styled.div<BoxProps>(
@@ -83,3 +229,5 @@ export const Box = styled.div<BoxProps>(
   },
   boxStyles,
 )
+
+Box.displayName = 'Box'
