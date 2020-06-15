c15bfaed37edae11fc01e640ec55161f01814f37
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2020-06-15 22:07:12 +0200

    add why-did-you-rerender

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import '@public/fonts/fonts.css'
 
+import whyDidYouRender from '@welldone-software/why-did-you-render'
 import { DefaultSeo } from 'next-seo'
+import { AppProps } from 'next/app'
 import { AppType } from 'next/dist/next-server/lib/utils'
+import React from 'react'
 import ReactModal from 'react-modal'
 import { ThemeProvider } from 'styled-components'
 import yn from 'yn'
@@ -11,15 +14,35 @@ import { ContextProvider } from '@/context/ContextProvider'
 import { GlobalStyle } from '@/theme/GlobalStyle'
 import { theme } from '@/theme/theme'
 import { appConfigurator } from '@/utils/appConfigurator'
+import { isBrowser } from '@/utils/isBrowser'
 
 if (process.browser) {
   require('@/utils/detectTouch')
   require('@/utils/detectKeyboardFocus')
 }
 
+if (isBrowser && process.env.ENVIRONMENT_NAME !== 'production') {
+  /**
+   * why-did-you-render README
+   * https://github.com/welldone-software/why-did-you-render
+   */
+
+  whyDidYouRender(React)
+
+  /**
+   * To debug a component:
+   *
+   * const Whatever = () => <Box />
+   *
+   * Whatever.whyDidYouRender = true
+   *
+   * export { Whatever }
+   */
+}
+
 ReactModal.setAppElement('#__next')
 
-const MyApp: AppType = ({ Component: Page, pageProps }) => {
+const MyApp: AppType = ({ Component: Page, pageProps }: AppProps) => {
   return (
     <>
       <DefaultSeo titleTemplate={`%s | Dept`} />
