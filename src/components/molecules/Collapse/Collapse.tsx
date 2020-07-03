037001c664ae16c7d54ba6eeccfd53ea91d01a70
Robby Uitbeijerse <robby.uitbeijerse@deptagency.com>
2020-07-03 13:53:16 +0200

    Add collapse with framer

--- src/components/molecules/Collapse/Collapse.tsx
+++ src/components/molecules/Collapse/Collapse.tsx
@@ -1,61 +1,30 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import { FC } from 'react'
-import { CSSTransition } from 'react-transition-group'
-import styled from 'styled-components'
-
-import { useMeasure } from '@/utils/hooks'
 
 interface Props {
   isOpen?: boolean
   maxHeight?: number
 }
 
-const duration = 300
-
-const CollapseWrapper = styled.div<{ height: number }>`
-  overflow: hidden;
-  transition: opacity ${duration}ms 100ms, height ${duration}ms;
-  transition-timing-function: cubic-bezier(0.77, 0, 0.175, 1);
-  opacity: 0;
-  height: 0px;
-
-  &.collapse-enter {
-    opacity: 0;
-    height: 0px;
-  }
-  &.collapse-enter-active {
-    opacity: 1;
-    height: ${props => props.height}px;
-  }
-  &.collapse-enter-done {
-    opacity: 1;
-    height: auto;
-    overflow: auto;
-  }
-  &.collapse-exit {
-    opacity: 1;
-    height: ${props => props.height}px;
-  }
-  &.collapse-exit-active {
-    opacity: 0;
-    height: 0px;
-  }
-  &.collapse-exit-done {
-    opacity: 0;
-    height: 0px;
-  }
-`
+const duration = 0.8
 
 export const Collapse: FC<Props> = ({ children, isOpen = false }) => {
-  const { ref, bounds } = useMeasure()
-  const { height: elementHeight } = bounds
-
   return (
-    <>
-      <CSSTransition appear classNames="collapse" in={isOpen} timeout={duration}>
-        <CollapseWrapper height={elementHeight}>
-          <div ref={ref}>{children}</div>
-        </CollapseWrapper>
-      </CSSTransition>
-    </>
+    <AnimatePresence initial={false}>
+      {isOpen && (
+        <motion.section
+          key="content"
+          initial="collapsed"
+          animate="open"
+          exit="collapsed"
+          variants={{
+            open: { opacity: 1, height: 'auto' },
+            collapsed: { opacity: 0, height: 0 },
+          }}
+          transition={{ duration, ease: [0.04, 0.62, 0.23, 0.98] }}>
+          {children}
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
