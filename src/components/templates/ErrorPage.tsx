ef1c9a01e594ec051069731dc0aaab87d34398b2
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2020-08-31 12:29:58 +0200

    adjust the error page to be functional

--- src/components/templates/ErrorPage.tsx
+++ src/components/templates/ErrorPage.tsx
@@ -1,9 +1,10 @@
 import { Heading, Paragraph } from '@/components/atoms';
+import { ErrorProps } from '@/pages/_error';
 
-export const ErrorPage: React.FC = () => {
+export const ErrorPage: React.FC<ErrorProps> = ({ statusCode }) => {
   return (
     <>
-      <Heading>Oops...</Heading>
+      <Heading>Oops... {statusCode ? `| ${statusCode} error` : ''} </Heading>
       <Paragraph>Something went wrong</Paragraph>
     </>
   );
--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,21 +1,19 @@
-import { NextPageContext } from 'next';
+import { NextPage, NextPageContext } from 'next';
 import React from 'react';
 
 import { ErrorPage } from '@/components/templates';
 
-interface ErrorProps {
-  statusCode: number | null;
+export interface ErrorProps {
+  statusCode?: number | null;
 }
 
-class MyError extends React.Component<ErrorProps> {
-  static getInitialProps({ res, err }: NextPageContext) {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-    return { statusCode };
-  }
+const Error: NextPage<ErrorProps> = ({ statusCode }) => {
+  return <ErrorPage {...{ statusCode }} />;
+};
 
-  render() {
-    return <ErrorPage />;
-  }
-}
+Error.getInitialProps = async ({ res, err }: NextPageContext) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+  return { statusCode };
+};
 
-export default MyError;
+export default Error;
