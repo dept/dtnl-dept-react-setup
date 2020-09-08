96f0b546945b296310359178eb87b0b981272ed9
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2020-09-08 12:27:45 +0200

    dept-react > dept-react-scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,9 @@
  * The lint rules are extended from `dept-react-scripts`.
  * You can add or overwrite rules if you want to.
  *
- * If you wish to eject from `dept-react-script` run `npx dept-react eslint-eject`
+ * If you wish to eject from `dept-react-script` run `npx dept-react-scripts eslint-eject`
  */
 
 module.exports = {
   extends: ['./node_modules/dept-react-scripts/config/eslint'],
-}
+};
