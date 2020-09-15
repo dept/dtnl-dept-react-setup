e7608bda79c1eac2896e4b44b06ad84b657e8788
Tim de Wolf <tpdewolf@gmail.com>
2020-09-15 13:54:55 +0200

    remove singleline in favor of 'truncate' prop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import React from 'react';
 
-import { Box, Heading, Paragraph, Text } from '@/components/atoms';
+import { Box, Heading, Paragraph } from '@/components/atoms';
 
 interface PageProps {}
 
@@ -14,7 +14,7 @@ const Page: NextPage<PageProps> = () => {
         <Heading as="h1" color="primary" variant="heading1">
           Homepage
         </Heading>
-        <Text>Run `yarn storybook` to view all components</Text>
+        <Paragraph>Run `yarn storybook` to view all components</Paragraph>
         <Paragraph>Run `yarn route [name]` to create a page</Paragraph>
         <Paragraph>Run `yarn component [name]` to create a component</Paragraph>
         <Paragraph>Run `yarn context [name]` to create a context provider</Paragraph>
