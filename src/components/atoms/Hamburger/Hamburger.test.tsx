9d3013671cd7501f7c30313ed8dbd185296f7d9e
Tim de Wolf <tpdewolf@gmail.com>
2020-10-30 16:02:27 +0100

    going vegan. screw the hamburger

--- src/components/atoms/Hamburger/Hamburger.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { render } from '@test/utils';
-
-import { Hamburger } from './Hamburger';
-
-test('it renders', () => {
-  render(<Hamburger onClick={console.log} isActive={false}></Hamburger>);
-});
