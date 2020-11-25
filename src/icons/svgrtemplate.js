62a16e0c4c3c23f6f469687527e6b67e9dffd754
Damian Veltkamp <damianveltkamp@Damians-MacBook-Pro-2.local>
2020-11-25 14:11:19 +0100

    Added functionality to set colors with variables on svg

--- src/icons/svgrtemplate.js
+++ src/icons/svgrtemplate.js
@@ -4,18 +4,29 @@ function template({ template }, _opts, { componentName, jsx }) {
   componentName.name = componentName.name.slice(3) + 'Icon';
 
   return typeScriptTpl.ast`
+  import React from 'react';
+  import styled from 'styled-components';
+  import { compose, color } from 'styled-system';
+
   interface CustomIconProps extends React.SVGProps<SVGSVGElement> {
     size?: number
   }
 
-  const ${componentName} = ({ size, ...props }: CustomIconProps) => {
+  const svgStyles = compose(
+    color
+  )
+
+  const SVG = ({ size, ...props }: CustomIconProps) => {
     if (size) {
-      props.width = size;
-      props.height = size;
+      props.width = size
+      props.height = size
     }
 
     return (${jsx})
-  };
+  }
+
+  const ${componentName} = styled(SVG)(svgStyles)
+
 
   export default ${componentName}
 `;
