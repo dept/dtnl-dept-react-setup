060f2f363ecfb1dc760e27ab21a4db7e5469548c
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2020-11-28 14:01:42 +0100

    move rerender

--- src/components/atoms/Debug/Rerender.tsx
+++ src/components/atoms/Debug/Rerender.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 
-export const RenderCount = () => {
+export const Rerender = () => {
   const renders = useRef(0);
 
   return <>Rerender count: {++renders.current}</>;
