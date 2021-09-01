63e4069239b1586535676a68e146edf4e7c24ab6
mattvanvoorst <matt.vanvoorst@deptagency.com>
2021-09-01 12:29:22 +0200

    fix: Babel extend & FS support: Babel extend & FS support
    Storybook now extends the default babel config and mocks the fs incase of usage in any of the components now

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    [
+      'next/babel',
+      {
+        'preset-react': {
+          runtime: 'automatic',
+          importSource: '@emotion/react',
+        },
+      },
+    ],
+  ],
+  plugins: [
+    ['@emotion/babel-plugin'],
+    [
+      'import',
+      {
+        libraryName: 'react-use',
+        libraryDirectory: 'lib',
+        camel2DashComponentName: false,
+      },
+    ],
+  ],
+};
