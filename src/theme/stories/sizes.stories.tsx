934ae2e2a2153c260fb6bbdf8217fde5955a41c2
Jeroen Reumkens <jeroen.reumkens@deptagency.com>
2021-12-21 17:17:00 +0100

    Moved theme stories to a seperate direcotry.

--- src/theme/stories/sizes.stories.tsx
+++ src/theme/stories/sizes.stories.tsx
@@ -1,6 +1,4 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
-
-import { colors } from '../theme/colors';
+import { Heading, Text } from '@chakra-ui/react';
 
 export default { title: 'Tokens/Typography', component: null };
 
@@ -35,34 +33,5 @@ export const headingsAndText = () => (
     <Text fontSize="2xl">Paragraph</Text>
     <Text fontSize="3xl">Paragraph</Text>
     <Text fontSize="4xl">Paragraph</Text>
-  </>
-);
-
-export const customColors = () => (
-  <>
-    {Object.entries(colors).map(([colorName, colorValue]) => (
-      <Flex
-        key={colorName}
-        backgroundColor="white"
-        shadow="md"
-        borderRadius="lg"
-        display="inline-flex"
-        width="150px"
-        height="150px"
-        m="2"
-        flexDirection="column"
-        textAlign="center"
-      >
-        <Box
-          borderTopRadius="lg"
-          mb="2"
-          backgroundColor={colorValue as string}
-          height="100px"
-          borderBottom="1px solid #ccc"
-        ></Box>
-        {colorName}
-        <pre>{colorValue}</pre>
-      </Flex>
-    ))}
   </>
 );
