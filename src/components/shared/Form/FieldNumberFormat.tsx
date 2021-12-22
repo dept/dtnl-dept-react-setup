b1779d9a0670bc21072370b5ad8752edb7d2a435
Jeroen Reumkens <jeroen.reumkens@deptagency.com>
2021-12-22 09:17:16 +0100

    Kinda fixed typing issue, not investing more time since this will be deleted soon.

--- src/components/shared/Form/FieldNumberFormat.tsx
+++ src/components/shared/Form/FieldNumberFormat.tsx
@@ -2,7 +2,7 @@ import NumberFormat, { NumberFormatProps } from 'react-number-format';
 
 import { FieldInput, FieldInputProps } from './FieldInput';
 
-export type FieldNumberFormatProps = Omit<FieldInputProps, 'type'> & NumberFormatProps;
+export type FieldNumberFormatProps = Omit<FieldInputProps, 'type'> & NumberFormatProps<any>;
 
 export const FieldNumberFormat = (props: FieldNumberFormatProps) => {
   return <NumberFormat customInput={FieldInput} {...props} />;
