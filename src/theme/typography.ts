ffaf8799b9582f83a433506279bacbdab2fdd490
Jeroen Reumkens <jeroen.reumkens@deptagency.com>
2021-12-22 14:36:39 +0100

    Updated default typefaces.

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,7 +1,7 @@
 import { ChakraTheme } from '@chakra-ui/react';
 
 export const fonts: ChakraTheme['fonts'] = {
-  heading: `Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
-  body: `Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
-  mono: `SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace`,
+  heading: `Arial, sans-serif`,
+  body: `Arial, sans-serif`,
+  mono: `"Courier New", Courier, monospace`,
 };
