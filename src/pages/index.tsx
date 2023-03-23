f580604bd01d0bdbc2c1bc5e676587a229ecc3e4
Semsettin Bakkum <semsettin.bakkum@deptagency.com>
2023-03-23 12:39:59 +0100

    (feat): Put back old return for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,18 @@ import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
 
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
-const Page: NextPage<PageProps> = () => {
-  return (
-    <>
-      <Stack spacing="4">
-        <Heading as="h1" size="h1" color="brand.han-purple">
-          Homepage
-        </Heading>
-        <Text textStyle="h1">Run `yarn storybook` to view all components</Text>
-        <Text>Run `yarn route [name]` to create a page</Text>
-        <Text>Run `yarn component [name]` to create a component</Text>
-        <Text>Run `yarn context [name]` to create a context provider</Text>
-        {process.env.ENVIRONMENT_NAME && (
-          <code>Running on environment: {process.env.ENVIRONMENT_NAME}</code>
-        )}
-      </Stack>
-    </>
-  );
-};
+const Page: NextPage<PageProps> = () => (
+  <Stack spacing="4">
+    <Heading as="h1">Homepage</Heading>
+    <Text>Run `yarn storybook` to view all components</Text>
+    <Text>Run `yarn route [name]` to create a page</Text>
+    <Text>Run `yarn component [name]` to create a component</Text>
+    <Text>Run `yarn context [name]` to create a context provider</Text>
+    {process.env.ENVIRONMENT_NAME && (
+      <code>Running on environment: {process.env.ENVIRONMENT_NAME}</code>
+    )}
+  </Stack>
+);
 
 export const getStaticProps = (() => {
   return {
