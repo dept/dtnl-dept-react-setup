a99868667f0313184b9b55e15ab7cd2a88d025a4
Semsettin Bakkum <semsettin.bakkum@deptagency.com>
2023-03-30 13:59:25 +0200

    feat: changed isBrowser import to fetch from @dept/ui

--- apps/web/src/utils/withUniversalCookies.tsx
+++ apps/web/src/utils/withUniversalCookies.tsx
@@ -3,7 +3,7 @@ import { AppContext, AppProps } from 'next/app';
 import { AppType } from 'next/dist/shared/lib/utils';
 import { Cookies, CookiesProvider } from 'react-cookie';
 
-import { isBrowser } from './isBrowser';
+import { isBrowser } from '@dept/ui';
 
 type WithUniversalCookieProps = AppProps & {
   cookies: Cookies;
