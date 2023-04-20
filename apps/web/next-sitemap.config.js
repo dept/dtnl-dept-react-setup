90a713f99c7b0062f5cd3b1313187b2a7e271d6f
Semsettin Bakkum <semsettin.bakkum@deptagency.com>
2023-04-20 17:30:16 +0200

    feat(sitemap): working on env sitemap fix

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 
 const config = {
-  siteUrl: process.env.NEXT_PUBLIC_APP_URL,
+  siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
   exclude: ['/unsupported'],
 };
 
-module.exports = config;
+module.exports = { ...config };
